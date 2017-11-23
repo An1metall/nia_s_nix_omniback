67136f640b355e6b3d0e0a5b4554608051737b08
Roman Bardashov <bardashovrv@gmail.com>
2017-11-24 01:10:03 +0300

    Started simple http server

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,28 @@
-print("Hi there!")
+//
+//  main.swift
+//  nia_s_nix_omniback
+//
+//  Created by Roman Bardashov on 22.10.17.
+//  Copyright © 2017 Roman Bardashov. All rights reserved.
+//
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+let server = HTTPServer()
+
+var routes = Routes()
+routes.add(method: .get, uri: "/") { (request, response) in
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+    response.completed()
+}
+
+server.addRoutes(routes)
+server.serverPort = 8181
+
+do {
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
